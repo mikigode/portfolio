a836df758b834d3303fe070b3078871cdef42674
MikiGode <82653151+mikigode@users.noreply.github.com>
2022-04-02 16:40:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -41,6 +41,13 @@ $(document).ready(function() {
         loop: true
     });
 
+    var typed = new Typed(".typingAM", {
+        strings: ["ዩቱበር", "አበልፃጊ", "ብሎግገር", "አቀናባሪ", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });    
+
     var typed = new Typed(".typing-2", {
         strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
         typeSpeed: 100,
